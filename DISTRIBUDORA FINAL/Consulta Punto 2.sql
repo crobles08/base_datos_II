2f2675b1a09d08eb0e4a41210b02a21f86df9731
Cristian Robles <cristianrobles2020@itp.edu.co>
2022-03-09 19:28:57 -0500

    Creando Repositorio

---
+++ DISTRIBUDORA FINAL/Consulta Punto 2.sql
@@ -0,0 +1,8 @@
+SELECT sc.`id_sucursal` AS ID_SUCURSAL, sucursal.`nombre_encargado` AS ENCARGADO_SUCURSAL, CONCAT( "(",contactos.`identificacion`, ") ", contactos.`nom_representante`)AS NOMBRE_CLIENTE, paises.`n_pais` AS PAIS
+FROM sucursal_cliente sc 
+INNER JOIN sucursal ON sucursal.`id` = sc.`id_sucursal`
+INNER JOIN contactos ON contactos.`id` = sc.`id_contacto`
+INNER JOIN ciudades ON sucursal.`id_ciudad_u` = ciudades.`id`
+INNER JOIN departamentos ON ciudades.`id_departamento`=departamentos.`id`
+INNER JOIN paises ON departamentos.`id_pais` = paises.`id`
+GROUP BY sc.`id_sucursal`
